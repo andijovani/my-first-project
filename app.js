8c20a88d6a4f4065d76a25150f906270c2bc998b
andijovani <Andi0Titan@hotmail.com>
2018-10-26 19:50:12 +0200

    Added comment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var names = ["Andi", "Mateo", "Don", "Jenny"]
 
 function displayName() {
     button.addEventListener("click", function(){
-        input.value = names[Math.round( Math.random() * 3 )]
+        input.value = names[Math.round( Math.random() * 3 )] //random name
     })
 }
 
