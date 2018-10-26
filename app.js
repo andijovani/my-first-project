4d7652ac024507d77330d9f1128e1bd566fa7328
andijovani <Andi0Titan@hotmail.com>
2018-10-26 19:46:13 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("submit")
+var input = document.getElementById("name")
+
+var names = ["Andi", "Mateo", "Don", "Jenny"]
+
+function displayName() {
+    button.addEventListener("click", function(){
+        input.value = names[Math.round( Math.random() * 3 )]
+    })
+}
+
+displayName()
